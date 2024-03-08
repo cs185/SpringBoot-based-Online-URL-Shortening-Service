ecf3e3dbad4e4054fef0d5da78156adadc4d3ee2
kaushalag29 <kaushalag29@gmail.com>
2024-03-08 15:16:14 -0600

    Adding basic template with jdk 21

---
+++ src/main/java/com/example/semproject/UrlController.java
@@ -0,0 +1,49 @@
+package com.example.semproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UrlController {
+
+//    @Autowired
+//    private ShrinkService service;
+//
+//    @PostMapping(value = "shrink")
+//    public ResponseEntity<?> shortenUrl(@RequestBody ShrinkRequest request) {
+//        try {
+//            ShrinkResponse response = service.shrinkUrl(request);
+//            HttpStatus status = (response.isValidUrl() && StringUtils.isBlank(response.getErrorDesc())) ? HttpStatus.OK : HttpStatus.BAD_REQUEST;
+//            return new ResponseEntity<ShrinkResponse>(response, status);
+//        } catch (Exception e) {
+//            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//
+//    }
+//
+//    @GetMapping(value = "{shrinkKey}")
+//    public ResponseEntity<?> getUrlAndRedirect(@PathVariable String shrinkKey) {
+//        try {
+//            String url = service.getFullUrl(shrinkKey);
+//
+//            String response = StringUtils.isBlank(url) ? "URL_NOT_FOUND" : url;
+//            HttpStatus status = StringUtils.isBlank(url) ? HttpStatus.NOT_FOUND : HttpStatus.OK;
+//
+//            if (status.is2xxSuccessful())
+//                return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(response)).build();
+//            else
+//                return new ResponseEntity<String>(response, status);
+//        } catch (Exception e) {
+//            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//    }
+
+    @GetMapping("/register")
+    public ModelAndView showWebPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("register.html");
+        return modelAndView;
+    }
+}
+
---
+++ src/test/java/com/example/semproject/SemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
